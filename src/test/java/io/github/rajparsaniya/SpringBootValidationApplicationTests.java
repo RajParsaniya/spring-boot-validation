f8779ab1432bd14527ec06b09a378b21412b4939
Raj Parsaniya <raj.parsaniya@brevitaz.com>
2024-02-16 17:44:33 +0530

    Initial commit of spring boot project

---
+++ src/test/java/io/github/rajparsaniya/SpringBootValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rajparsaniya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
