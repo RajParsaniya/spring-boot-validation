49f6161919c4c973ebc82bac79ce5c69d8858f0e
Raj Parsaniya <rajparsaniya55@gmail.com>
2024-06-20 09:45:17 +0530

    SpringBoot project created

---
+++ src/main/java/io/github/rajparsaniya/SpringBootValidationApplication.java
@@ -0,0 +1,11 @@
+package io.github.rajparsaniya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootValidationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootValidationApplication.class, args);
+    }
+}
